b2211a02238c81a1f21649aae260babf7b9b359d
Paul Joannon <hello@pauljoannon.com>
2018-01-13 22:48:38 +0100

    add help to github command

--- commands/github.js
+++ commands/github.js
@@ -1,5 +1,5 @@
 module.exports.command  = 'github'
-module.exports.help     = ''
+module.exports.help     = 'Envoie l\'adresse du dépôt GitHub de mon code source.'
 module.exports.callback = (message, words) => {
     message.reply(`<https://github.com/paulloz/bip-boup.git>`);
 };
