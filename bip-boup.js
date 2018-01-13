d0b75e9be0b07da96902aab0318bfbecda8413c0
Paul Joannon <hello@pauljoannon.com>
2018-01-13 20:18:54 +0100

    properly disconnect on ctrl-c

--- bip-boup.js
+++ bip-boup.js
@@ -3,8 +3,16 @@ const Fs = require('fs');
 const Discord = require('discord.js');
 const bipboup = new Discord.Client();
 
+console.log('Starting up...');
+
 bipboup.on('ready', () => {
-    console.log('READY!');
+    console.log('I\'m connected to the Discord guild!');
+
+    // Properly close connection on Ctrl-C
+    process.on('SIGINT', () => {
+        console.log('Shuting down...');
+        bipboup.destroy().then(() => process.exit());
+    });
 });
 
 Fs.readFile('.tosken', { encoding : 'utf-8' }, (err, data) => {
