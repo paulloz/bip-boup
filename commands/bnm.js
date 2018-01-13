365156f45ee4c83787b17ede43d87ffb5a12e811
louckousse <louckousse@gmail.com>
2018-01-14 00:12:22 +0100

    Add a space

--- commands/bnm.js
+++ commands/bnm.js
@@ -2,5 +2,5 @@ module.exports.command  = 'note'
 module.exports.help     = 'Donne une note imparatiale sur un objet culturel.'
 module.exports.callback = (message, words) => {
     const notes = ["bnm nrv", "bnm neutre", "6/10 très bon", "FFXV GOTY", "C DE LA MERDE"];
-    message.reply(words.length > 0 ? words[1] : "" + notes[Math.floor(Math.random() * Math.floor(notes.length))]);
+    message.reply(words.length > 0 ? words[1] + " " : "" + notes[Math.floor(Math.random() * Math.floor(notes.length))]);
 };
