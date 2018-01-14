6fbf814c0d3b720da40c9d13b025b45104d53af8
Paul Joannon <hello@pauljoannon.com>
2018-01-14 20:20:50 +0100

    minor changes before merge

--- commands/note.js
+++ commands/note.js
@@ -4,22 +4,23 @@ module.exports.command  = 'note'
 module.exports.help     = 'Donne une note imparatiale sur un objet culturel.'
 module.exports.callback = (message, words) => {
     const notes = [
-                   "bnm nrv", "bnm neutre",
-                   `6/10 très bon ${EmojiOrNothing(message.channel, 'dreamburger')}`,
-                   "FFXV GOTY", "C DE LA MERDE",
-                   `pour les fans de ${EmojiOrNothing(message.channel, 'jul')}`,
-                   "bnm content", "rice", "TÛT TÛT LES JALOUX", "Telerama a aimé, moi non",
-                   "Ça vaut vraiment pas le premier...", "chef d'œuvre", "vous feriez mieux de regarde Mad Max",
-                   "quelques rares défauts mais un excellent moment",
-                   `${EmojiOrNothing(message.channel, 'baby')} qui pleure`,
-                   "RT car c triss", "j'en sais rien, je connais pas", "bon somnifère", "mieux que du café"
-               ];
+        "bnm nrv", "bnm neutre", "bnm content",
+        `6/10 très bon ${EmojiOrNothing(message.channel, 'dreamburger')}`,
+        "FFXV GOTY", "C DE LA MERDE",
+        `pour les fans de ${EmojiOrNothing(message.channel, 'jul')}`,
+        "bnm content", ["8/10", "with rice 10/10"], "TÛT TÛT LES JALOUX", "Telerama a aimé, moi non",
+        "Ça vaut vraiment pas le premier...", "chef d'œuvre", "vous feriez mieux de regarder Mad Max",
+        "quelques rares défauts mais un excellent moment",
+        `${EmojiOrNothing(message.channel, 'baby')} qui pleure`,
+        "RT car c triss", "j'en sais rien, je connais pas", "bon somnifère", "mieux que du café"
+    ];
+
     const prefix = words.length >= 2 ? words.slice(1).join(' ') + ": " : "";
-    let seed = Math.floor(Math.random() * Math.floor(notes.length));
-    if (notes[seed] === "rice") {
-        message.channel.send(prefix + "8/10");
-        message.channel.send(prefix + "with rice 10/10");
+    const n = Math.floor(Math.random() * Math.floor(notes.length));
+
+    if (typeof(notes[n]) === typeof([])) {
+        notes[n].forEach(note => message.channel.send(prefix + note));
     } else {
-        message.channel.send(prefix + notes[seed]);
+        message.channel.send(prefix + notes[n]);
     }
 };
