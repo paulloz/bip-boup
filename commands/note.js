6bedd109ddcd70f4a17ef4f33c6c4a8d0db69a80
Paul Joannon <hello@pauljoannon.com>
2018-01-14 13:59:44 +0100

    fix a typo

--- commands/note.js
+++ commands/note.js
@@ -1,7 +1,7 @@
 module.exports.command  = 'note'
 module.exports.help     = 'Donne une note imparatiale sur un objet culturel.'
 module.exports.callback = (message, words) => {
-    const notes = ["bnm nrv", "bnm neutre", "6/10 très bon :dreambuger:", "FFXV GOTY", "C DE LA MERDE"];
+    const notes = ["bnm nrv", "bnm neutre", "6/10 très bon :dreamburger:", "FFXV GOTY", "C DE LA MERDE"];
     const prefix = words.length >= 2 ? words.slice(1).join(' ') + ": " : "";
     message.channel.send(prefix + notes[Math.floor(Math.random() * Math.floor(notes.length))]);
 };
