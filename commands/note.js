7c3380b57c5b18427cab166a888e7b2e796fa4bf
Paul Joannon <hello@pauljoannon.com>
2018-01-14 14:36:34 +0100

    use emojis as intended in !note command

--- commands/note.js
+++ commands/note.js
@@ -1,7 +1,11 @@
+const { EmojiOrNothing } = require('../utils.js');
+
 module.exports.command  = 'note'
 module.exports.help     = 'Donne une note imparatiale sur un objet culturel.'
 module.exports.callback = (message, words) => {
-    const notes = ["bnm nrv", "bnm neutre", "6/10 très bon :dreamburger:", "FFXV GOTY", "C DE LA MERDE"];
+    const notes = ["bnm nrv", "bnm neutre",
+                   `6/10 très bon ${EmojiOrNothing(message.channel, 'dreamburger')}`,
+                   "FFXV GOTY", "C DE LA MERDE"];
     const prefix = words.length >= 2 ? words.slice(1).join(' ') + ": " : "";
     message.channel.send(prefix + notes[Math.floor(Math.random() * Math.floor(notes.length))]);
 };
