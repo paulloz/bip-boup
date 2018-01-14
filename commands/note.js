80ab447efa4727f40c5528027d37c6cf7836f6f6
Paul Joannon <hello@pauljoannon.com>
2018-01-14 13:57:56 +0100

    can !note nothing

--- commands/note.js
+++ commands/note.js
@@ -2,6 +2,6 @@ module.exports.command  = 'note'
 module.exports.help     = 'Donne une note imparatiale sur un objet culturel.'
 module.exports.callback = (message, words) => {
     const notes = ["bnm nrv", "bnm neutre", "6/10 très bon :dreambuger:", "FFXV GOTY", "C DE LA MERDE"];
-    if (words.length < 2) return;
-    message.channel.send(words.slice(1).join(' ') + ": " + notes[Math.floor(Math.random() * Math.floor(notes.length))]);
+    const prefix = words.length >= 2 ? words.slice(1).join(' ') + ": " : "";
+    message.channel.send(prefix + notes[Math.floor(Math.random() * Math.floor(notes.length))]);
 };
