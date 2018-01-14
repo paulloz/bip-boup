5ebe214ac303beda5171a1938998c5686a7abb6a
Paul Joannon <hello@pauljoannon.com>
2018-01-14 13:53:27 +0100

    fix :poop: from previous merge
    learn 2 git plz thx

--- commands/bnm.js
+++
@@ -1,6 +0,0 @@
-module.exports.command  = 'note'
-module.exports.help     = 'Donne une note imparatiale sur un objet culturel.'
-module.exports.callback = (message, words) => {
-    const notes = ["bnm nrv", "bnm neutre", "6/10 très bon", "FFXV GOTY", "C DE LA MERDE"];
-    message.reply(words.length > 0 ? words[1] + " " : "" + notes[Math.floor(Math.random() * Math.floor(notes.length))]);
-};
