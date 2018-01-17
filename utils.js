448dc438cd8f9236dc8517922d7469dfceac3190
Paul Joannon <hello@pauljoannon.com>
2018-01-17 11:46:55 +0100

    create Utils.HttpsGet

--- utils.js
+++ utils.js
@@ -1,1 +1,15 @@
+const Https = require('https');
+
 module.exports.EmojiOrNothing = (channel, emoji) => channel.guild != null ? channel.guild.emojis.find('name', emoji) || '' : ''
+
+module.exports.HttpsGet = (url, callback) => {
+    return Https.get(url, result => {
+        result.setEncoding('utf8');
+
+        let htmlBody = '';
+        result.on('data', data => htmlBody += data);
+        result.on('end', () => {
+            callback(htmlBody);
+        });
+    });
+};
