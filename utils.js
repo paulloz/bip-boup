a8357ac9dccb2c7ce1381ea29b7251c79faf52c9
Paul Joannon <hello@pauljoannon.com>
2018-01-17 13:05:35 +0100

    use URL object and set User-Agent when doing an HTTP Get

--- utils.js
+++ utils.js
@@ -1,9 +1,20 @@
 const Https = require('https');
+const { URL } = require('url');
 
 module.exports.EmojiOrNothing = (channel, emoji) => channel.guild != null ? channel.guild.emojis.find('name', emoji) || '' : ''
 
 module.exports.HttpsGet = (url, callback) => {
-    return Https.get(url, result => {
+    // Need an URL object
+    if (typeof(url) == typeof(''))
+        url = new URL(url);
+
+    return Https.get({
+        hostname: url.hostname,
+        path: url.pathname,
+        headers: {
+            'User-Agent': 'Bip Boup/1.0.0'
+        }
+    }, result => {
         result.setEncoding('utf8');
 
         let htmlBody = '';
