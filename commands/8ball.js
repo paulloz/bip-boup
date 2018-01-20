7898bc3fd9b16e484608452e1f13f3206156ebd9
louckousse <louckousse@gmail.com>
2018-01-20 23:23:00 +0100

    New command featuring a :8ball:

---
+++ commands/8ball.js
@@ -0,0 +1,30 @@
+module.exports.command  = '8ball'
+module.exports.help     = 'Ton meilleur conseiller.'
+module.exports.callback = (message, words) => {
+    const fortunes = [
+        "Essaye plus tard","Essaye encore","Pas d'avis","C'est ton destin","Le sort en est jeté",
+        "Une chance sur deux","Repose ta question","D'après moi oui","C'est certain","Oui absolument",
+        "Tu peux compter dessus","Sans aucun doute","Très probable","Oui","C'est bien parti","C'est non",
+        "Peu probable","Faut pas rêver","N'y compte pas","Impossible"
+    ];
+
+    let n = 5;
+
+    for (var i = 0; i < 10; i++) {
+        roll(i*500,n);
+        n += Math.floor(Math.random() * Math.floor(5)) - 2;
+    }
+
+    function roll(wt, n) {
+        setTimeout(function() {
+            let str = ".";
+            for(var i = 0; i < n; i++) {
+                str+= " ";
+            }
+            message.channel.send(str + ":right_facing_fist::8ball::left_facing_fist:");
+            if (wt >= 4500) {
+                message.reply(fortunes[Math.floor(Math.random() * Math.floor(fortunes.length))]);
+            }
+        }, wt);
+    }
+};
