099ce7444fc4492bbc5b964656cd60867bc7beec
Paul Joannon <hello@pauljoannon.com>
2018-01-20 21:54:27 +0100

    remove dumb code in !rappel

--- commands/rappel.js
+++ commands/rappel.js
@@ -4,33 +4,10 @@ const moment = require('moment');
 module.exports.command = 'rappel';
 module.exports.help = 'Renvoie un message après un temps défini.';
 
-const quotes = ['\'', '"', '`', '«»', '“”', '‘’', '‹›'];
-
 module.exports.callback = (message, words) => {
     let messageContent;
 
     words = words.slice(1).join(' ');
-
-    // TODO This is dumb, do better
-    if (messageContent == null) {
-        let quoted = [-1, -1];
-
-        for (let quote of quotes) {
-            let startQuote = quote[0], endQuote = quote[quote.length > 1 ? quote.length - 1 : 0];
-            let startIdx = words.indexOf(startQuote);
-            if (startIdx >= 0 && (quoted[0] < 0 || startIdx < quoted[0])) {
-                let endIdx = words.lastIndexOf(endQuote)
-                if (endIdx >= 0 && endIdx > startIdx) {
-                    quoted = [startIdx, endIdx];
-                }
-            }
-        }
-
-        if (quoted[0] >= 0 && quoted[1] >= 0) {
-            messageContent = words.substring(quoted[0], quoted[1] + 1);
-            words = ((quoted[0] > words.length - quoted[1]) ? words.substring(0, quoted[0]) : words.substring(quoted[1] + 1)).trim();
-        }
-    }
 
     let matches = chrono.parse(words);
     if (matches.length > 0) {
