bc615505de6add2b61523c2892eb8bf140c83606
Paul Joannon <hello@pauljoannon.com>
2018-01-28 12:18:49 +0100

    create pp dir if needed

--- commands/pp.js
+++ commands/pp.js
@@ -9,6 +9,10 @@ module.exports.help = '';
 module.exports.admin = true;
 module.exports.setup = true;
 module.exports.callback = (bipboup) => {
+    const ppdir =  Path.join(__dirname, '..', 'pp');
+    if (!Fs.existsSync(ppdir))
+        Fs.mkdirSync(ppdir);
+
     return (message, words) => {
         if (message.attachments.array().length > 0) {
             let ok = false;
@@ -31,13 +35,13 @@ module.exports.callback = (bipboup) => {
                         result.on('end', () => {
                             let filename = attachment.filename;
                             let ext = Path.extname(filename);
-                            let path = Path.join(__dirname, '..', 'pp', filename);
+                            let path = Path.join(ppdir, filename);
                             while (Fs.existsSync(path)) {
                                 filename = Path.basename(filename, ext).split('-');
                                 filename = filename.length > 1
                                     ? [filename[0]].concat([String(parseInt(filename[1]) + 1)]).join('-')
                                     : filename.concat(['1']).join('-')
-                                path = Path.join(__dirname, '..', 'pp', filename + ext);
+                                path = Path.join(ppdir, filename + ext);
                             }
 
                             Fs.writeFileSync(path, data.read(), { encoding: 'binary', flag: 'w+' });
