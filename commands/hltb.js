078827161baa90671c1b70b96ab574b3d5bc75d0
Gautier de Saint Martin Lacaze <dark.jabberwock+github@gmail.com>
2019-01-11 21:37:21 +0100

    add hltb command (#1)
    * add hltb command
    
    * remove words from parameters
    
    Co-Authored-By: jabby <dark.jabberwock+github@gmail.com>

---
+++ commands/hltb.js
@@ -0,0 +1,6 @@
+module.exports.command  = 'hltb'
+module.exports.help     = 'HowLongToBeat'
+module.exports.callback = (message) => {
+    
+    message.reply("Le site est là : https://howlongtobeat.com/ \n Débrouille toi !");
+};
