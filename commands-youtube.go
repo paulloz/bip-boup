fd6696954eafc15ed5dae256e584685284b84216
Paul Joannon <hello@pauljoannon.com>
2019-01-31 13:47:23 +0100

    use util parse function in !nightcore

--- commands-youtube.go
+++ commands-youtube.go
@@ -2,31 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/moovweb/gokogiri"
 )
 
 func commandNightcore(args []string, env *CommandEnvironment) (*discordgo.MessageEmbed, string) {
-	if len(args) < 1 {
-		return nil, ""
-	}
-
-	resp, err := http.Get(fmt.Sprintf("%s&search_query=nightcore+%s", "https://www.youtube.com/results?sp=EgIQAQ%253D%253D", strings.Join(args, "+")))
-	if err != nil {
-		return nil, ""
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, ""
-	}
-
-	doc, err := gokogiri.ParseHtml(body)
+	doc, err := httpGetAsHtml(fmt.Sprintf("%s&search_query=nightcore+%s", "https://www.youtube.com/results?sp=EgIQAQ%253D%253D", strings.Join(args, "+")))
 	if err != nil {
 		return nil, ""
 	}
