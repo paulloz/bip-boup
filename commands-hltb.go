ac7553afef2ec752df5e2ae3399bd80d6c1a1882
louckousse <louckousse@flatbrains.me>
2019-02-06 16:42:12 +0100

    Use embedded response for a nicer display

--- commands-hltb.go
+++ commands-hltb.go
@@ -18,10 +18,18 @@ func commandHLTB(args []string, env *CommandEnvironment) (*discordgo.MessageEmbe
 	}
 	defer doc.Free()
 
+	imageResult := xpath.Compile("//div[@class='search_list_image']/a/img")
+	images, _ := doc.Root().Search(imageResult)
 	titlesResult := xpath.Compile("//div[@class='search_list_details']/h3")
 	titles, _ := doc.Root().Search(titlesResult)
 	gameResult := xpath.Compile("//div[@class='search_list_details_block']/div")
 	games, _ := doc.Root().Search(gameResult)
+
+	var image string
+	for _, img := range images {
+		image = img.Attr("src")
+		break
+	}
 
 	var title string
 	for _, titleFound := range titles {
@@ -29,39 +37,41 @@ func commandHLTB(args []string, env *CommandEnvironment) (*discordgo.MessageEmbe
 		break
 	}
 
+	if title == "" {
+		return nil, fmt.Sprintf("Aucune correspondance trouvée pour `%s`.", strings.Join(args, " "))
+	}
+
 	var content []string
 	for _, game := range games {
 		tmp := strings.Split(game.Content(), "\n")
-		content = []string{tmp[2], tmp[6]}
+		content = tmp[1:7]
 		break
 	}
 
-	return nil, formatResult(title, content, args)
+	return formatResult(title, image, content, args)
 }
 
-func formatResult(title string, times []string, args []string) string {
-	if title == "" {
-		return fmt.Sprintf("Aucun résultat pour %s.\n", strings.Join(args, " "))
+func formatResult(title string, imageURL string, content []string, args []string) (*discordgo.MessageEmbed, string) {
+	fields := []*discordgo.MessageEmbedField{}
+	for i := 0; i < len(content); i += 2 {
+		fields = append(fields, embedField(content[i], formatTime(content[i+1]), true))
 	}
-	result := strings.Trim(title, "\n")
-	if times[0] == "--" {
-		result += fmt.Sprintf(" n'a pas encore de temps renseigné")
-	} else {
-		result += fmt.Sprintf(" se finit en %s si tu traces", formatTime(times[0]))
-		if times[1] != "--" {
-			result += fmt.Sprintf(", %s si tu veux tout faire", formatTime(times[1]))
-		}
-	}
-	return fmt.Sprintf("%s.", result)
+
+	return &discordgo.MessageEmbed{
+		Title:       title,
+		Description: "",
+		Image:       &discordgo.MessageEmbedImage{URL: imageURL},
+		Fields:      fields,
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "Source : HowLongToBeat.com",
+		},
+	}, ""
+
 }
 
 func formatTime(time string) string {
-	tmp := strings.Split(time, " ")
-	if tmp[1] == "Hours" {
-		return fmt.Sprintf("%s %s", tmp[0], "h")
+	if time == "--" {
+		return "N/A"
 	}
-	if tmp[1] == "Mins" {
-		return fmt.Sprintf("%s %s", tmp[0], "m")
-	}
-	return ""
+	return time
 }
