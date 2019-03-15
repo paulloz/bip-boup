e6f97e0894a47e1d41ade158c4614df6cc5da480
Paul Joannon <hello@pauljoannon.com>
2019-03-15 22:04:35 +0100

    fix typo in !8ball

--- commands/commands-8ball.go
+++ commands/commands-8ball.go
@@ -17,7 +17,7 @@ func command8Ball(args []string, env *bot.CommandEnvironment, b *bot.Bot) (*disc
 
 	answers := []string{"Essaie plus tard.", "Essaie encore.", "J'ai pas d'avis.", "C'est ton destin.", "Le sort en est jeté.",
 		"Une chance sur deux.", "Repose ta question.", "D'après moi oui.", "C'est certain.", "Oui, absolument.",
-		"Tu peux compter dessus.", "Sans aucun doute.", "C'est très probable.", "Oui !", "C'est bien parti pour :wink:", "C'est non, désole…",
+		"Tu peux compter dessus.", "Sans aucun doute.", "C'est très probable.", "Oui !", "C'est bien parti pour :wink:", "C'est non, désolé…",
 		"Peu probable…", "Faut pas rêver…", "N'y compte même pas.", "Impossible !", "Je m'en fout complet, demande à quelqu'un d'autre et laisse moi pioncer bordel…"}
 
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
