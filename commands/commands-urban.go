acd1abd9218b9dced88931dde2a6c54fa99df223
Paul Joannon <hello@pauljoannon.com>
2019-03-15 22:04:49 +0100

    implements !urban

---
+++ commands/commands-urban.go
@@ -0,0 +1,47 @@
+package commands
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/moovweb/gokogiri/xpath"
+
+	"github.com/paulloz/bip-boup/bot"
+	"github.com/paulloz/bip-boup/httpreq"
+)
+
+func commandUrban(args []string, env *bot.CommandEnvironment, b *bot.Bot) (*discordgo.MessageEmbed, string) {
+	request := strings.Join(args, " ")
+
+	resp := &discordgo.MessageEmbed{
+		Title:       request,
+		Description: "Aucun résultat.",
+		Footer:      &discordgo.MessageEmbedFooter{Text: "Source: https://www.urbandictionary.com/"},
+	}
+
+	doc, err := httpreq.HTTPGetAsHTML(fmt.Sprintf("https://www.urbandictionary.com/define.php?term=%s", request))
+	if err != nil {
+		return resp, ""
+	}
+	defer doc.Free()
+
+	defs, _ := doc.Root().Search(xpath.Compile("//div[contains(@class, 'def-panel')]"))
+	if len(defs) > 0 {
+		meaning, _ := defs[0].Search(xpath.Compile("div[contains(@class, 'meaning')]"))
+
+		resp.Description = meaning[0].Content()
+	}
+
+	return resp, ""
+}
+
+func init() {
+	commands["urban"] = &bot.Command{
+		Function: commandUrban, HelpText: "Cherche une définition sur Urban Dictionary.",
+		Arguments: []bot.CommandArgument{
+			{Name: "requête", Description: "La définition à chercher", ArgType: "string"},
+		},
+		RequiredArguments: []string{"requête"},
+	}
+}
